60d50fc717afc19694cbcf74a30b929ed62a1293
Illin Tori <IllinTori@gmail.com>
2023-03-07 12:40:11 +0000

    Added Flask API

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
+
+# Dummy Data
+formats = ["wav"]
+dummy = {"file": "FrontEnd/src/Datastore/Happy V1.stl"}
+fail = dummy = {"file": ""}
+
+
+@app.route('/api/sculpt', methods=['POST'])
+def wav_to_model():
+    # Check if file present
+    if 'file' not in request.files:
+        return jsonify(fail), 400
+
+    # Load file from request
+    file = request.files['file']
+
+    if not (file and allowed_file(file.filename)):
+        # No valid file attached
+        return jsonify(fail), 400
+
+    # All validations passed, proced
+
+    # Process the data to make the model
+    data = dummy
+    
+    return jsonify(data), 200
+
+
+# Check if a given file is valid
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in formats
 
 # Function for root URL
 @app.route("/")
 def hello():
     return "Hello World!"
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
+    print("Fish bish")
