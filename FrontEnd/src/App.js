8bcff079f1c29f67b458233aca5009f31844f107
HRH00 <45105662+HRH00@users.noreply.github.com>
2023-04-07 12:40:50 +0100

    Add files via upload

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,64 +1,54 @@
-import React, {useState,} from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import {StlViewer} from "react-stl-viewer";
-import urlHappy from './Datastore/Happy V1.stl';
-import urlAngry from './Datastore/Angry V1.stl';
-import AudioComponent from './AudioComponent';
+import AudioComponent from './Components/AudioComponents/AudioComponent';
+import { ModelViewComponent } from '././Components/ModelComponents/ModelViewComponent';
+import placeHolderStl from './Datastore/WaitingForModel.stl'
 
 function App() {
+  const [activeView, setActiveView] = useState(1);
+  const [stlFile, setSTLFile] = useState(placeHolderStl);
+  const handleCallbackViewSwitch = (stlData) =>{
+    console.log("App.js handelcallback switch callled");
+    viewSwitch(stlData);
+  }
+  const handelCallbackSetSTL = (stlURI) => {
+    setSTLFile(stlURI);
+  }    
+  
   return (
     <body className="App-body">    
       <div className="App">
-        <header className='App-header'>
-          <div>
-            <h1>AI Audio Model Synthesiser</h1>
-          </div>
-        </header>
-        <AudioComponent/>
-        <ModelViewer></ModelViewer>
+        <header className='App-header'></header>
+        
+        <AudioComponent 
+          id="AudioComp"
+          viewCallback={handleCallbackViewSwitch} 
+          modelCallback={handelCallbackSetSTL} 
+          render={activeView}
+        />
+
+        <ModelViewComponent viewCallback={handleCallbackViewSwitch} url={stlFile} render={activeView}/>
+
       </div>    
     </body>
   );
+
+
+
+
+
+
+function viewSwitch(){
+  if (activeView===1){
+    setActiveView(0);
+  }
+  
+  else{
+    setActiveView(1)
+  }
 }
 
-function urlSwitch(url){
-  if (urlAngry===url){return urlHappy}
-  else{return urlAngry}
+
 }
-
-function StlViewerComponent(props){ 
-    const modelStyle = {
-      width: '100vw',
-      height: '75vh',
-    };
-
-    const modelProps={
-      color:'#6929C4'
-    };
-    
-    return (
-        <StlViewer
-          id='STLViewer'
-          style={modelStyle}
-          orbitControls
-          shadows={true}
-          url={props.url}
-          modelProps={modelProps} />
-    );
-}
-
-function ModelViewer() {
-    const [url, setUrl] = useState(urlAngry);
-    return (
-      <div>
-        <StlViewerComponent url={url}/>  
-        <button onClick={() => setUrl(urlSwitch(url))} >Click to Change Model</ button>
-        <button>
-          <a href={url} download>Download .STL</a>
-        </ button>
-      </div>
-    );
-}
-
 ReactDOM.render(<App/>,document.getElementById('root'));
 export default App;
